f3d58789ad6316ba3570108748229493e5d2ed80
l46kok <l46kok@localhost>
2016-08-27 22:42:02 -0600

    Added UART functionality, pinmux. Linked dependencies

---
+++ pinmux.c
@@ -0,0 +1,39 @@
+/*
+ * pinmux.c
+ *
+ *  Created on: 2016. 8. 27.
+ *      Author: Sokwhan
+ */
+
+
+#include "pinmux.h"
+#include "hw_types.h"
+#include "hw_memmap.h"
+#include "hw_gpio.h"
+#include "pin.h"
+#include "rom.h"
+#include "rom_map.h"
+#include "gpio.h"
+#include "prcm.h"
+
+//*****************************************************************************
+
+void
+PinMuxConfig(void)
+{
+    //
+    // Enable Peripheral Clocks
+    //
+    MAP_PRCMPeripheralClkEnable(PRCM_UARTA0, PRCM_RUN_MODE_CLK);
+
+    //
+    // Configure PIN_55 for UART0 UART0_TX
+    //
+    MAP_PinTypeUART(PIN_55, PIN_MODE_3);
+
+    //
+    // Configure PIN_57 for UART0 UART0_RX
+    //
+    MAP_PinTypeUART(PIN_57, PIN_MODE_3);
+}
+
