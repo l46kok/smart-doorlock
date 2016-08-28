64ca6eaf3fc96bdd50459ce614e0dc08267ce2d1
l46kok <2355949+l46kok@users.noreply.github.com>
2016-08-27 22:42:02 -0600

    Added UART functionality, pinmux. Linked dependencies

--- main.c
+++ main.c
@@ -1,7 +1,87 @@
 /*
  * main.c
  */
+
+// Driverlib includes
+#include "rom.h"
+#include "rom_map.h"
+#include "hw_memmap.h"
+#include "hw_common_reg.h"
+#include "hw_types.h"
+#include "hw_ints.h"
+#include "uart.h"
+#include "interrupt.h"
+#include "pinmux.h"
+#include "utils.h"
+#include "prcm.h"
+
+// Common interface include
+#include "uart_if.h"
+
+#define APPLICATION_VERSION  "1.1.1"
+#define APP_NAME             "UART Echo"
+#define CONSOLE              UARTA0_BASE
+#define UartGetChar()        MAP_UARTCharGet(CONSOLE)
+#define UartPutChar(c)       MAP_UARTCharPut(CONSOLE,c)
+#define MAX_STRING_LENGTH    80
+
+
+
+static void DisplayBanner(char * AppName)
+{
+
+    Report("\n\n\n\r");
+    Report("\t\t *************************************************\n\r");
+    Report("\t\t        CC3200 %s Application       \n\r", AppName);
+    Report("\t\t *************************************************\n\r");
+    Report("\n\n\n\r");
+}
+
+
+static void BoardInit(void)
+{
+/* In case of TI-RTOS vector table is initialize by OS itself */
+#ifndef USE_TIRTOS
+  //
+  // Set vector table base
+  //
+#if defined(ccs)
+    MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
+#endif
+#if defined(ewarm)
+    MAP_IntVTableBaseSet((unsigned long)&__vector_table);
+#endif
+#endif
+    //
+    // Enable Processor
+    //
+    MAP_IntMasterEnable();
+    MAP_IntEnable(FAULT_SYSTICK);
+
+    PRCMCC3200MCUInit();
+}
+
+
 int main(void) {
-	
+    char cString[MAX_STRING_LENGTH+1];
+    char cCharacter;
+    int iStringLength = 0;
+    //
+    // Initailizing the board
+    //
+    BoardInit();
+    //
+    // Muxing for Enabling UART_TX and UART_RX.
+    //
+    PinMuxConfig();
+    //
+    // Initialising the Terminal.
+    //
+    InitTerm();
+    //
+    // Clearing the Terminal.
+    //
+    ClearTerm();
+    DisplayBanner(APP_NAME);
 	return 0;
 }
---
+++ pinmux.h
@@ -0,0 +1,13 @@
+/*
+ * pinmux.h
+ *
+ *  Created on: 2016. 8. 27.
+ *      Author: Sokwhan
+ */
+
+#ifndef PINMUX_H_
+#define PINMUX_H_
+
+extern void PinMuxConfig(void);
+
+#endif /* PINMUX_H_ */
